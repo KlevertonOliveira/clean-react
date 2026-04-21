e68a464dd755dcf45f013654f40368779a3db5c9
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-04-21 10:52:50 -0300

    test: ensure AxiosHttpClient returns correct values

--- src/infra/axios-http-client/axios-http-client.spec.ts
+++ src/infra/axios-http-client/axios-http-client.spec.ts
@@ -1,11 +1,19 @@
 import { describe, test, expect, vi } from 'vitest';
-import { AxiosHttpClient } from './axios-http-client';
 import axios from 'axios';
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
+
+import { AxiosHttpClient } from './axios-http-client';
 import type { HttpPostParams } from '@/data/protocols/http';
 
 vi.mock('axios');
+
 const mockedAxios = vi.mocked(axios);
+const mockedAxiosResult = {
+  data: { content: faker.string.alphanumeric()},
+  status: faker.number.int()
+}
+
+mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
@@ -24,5 +32,16 @@ describe('AxiosHttpClient', () => {
     await sut.post(request)
 
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
-   })
+  });
+  
+  test('Should return the correct statusCode and body', async() => { 
+    const sut = makeSut();
+
+    const httpResponse = await sut.post(mockPostRequest());
+
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data
+    });
+  });
 })
