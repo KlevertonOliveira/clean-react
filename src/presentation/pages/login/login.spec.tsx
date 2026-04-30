574f1fdee0753b372c5f1f573d59a37a00bfab5b
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-04-29 21:40:01 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,16 +1,31 @@
 import { describe, expect, test } from "vitest";
 import { render, type RenderResult } from "@testing-library/react";
+import { userEvent } from '@testing-library/user-event'; 
 import LoginPage from "./login";
+import type { Validation } from "@/presentation/protocols/validation";
 
 type SutTypes = {
   sut: RenderResult;
+  validationSpy: ValidationSpy;
+}
+
+class ValidationSpy implements Validation {
+  errorMessage!: string;
+  input!: object;
+
+  validate(input: object): string {
+    this.input = input;
+    return this.errorMessage;
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<LoginPage />);
+  const validationSpy = new ValidationSpy()
+  const sut = render(<LoginPage validation={validationSpy}/>);
   
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
@@ -34,5 +49,22 @@ describe('Login Component', () => {
     const passwordInputStatus = sut.getByTestId('password-status');
     expect(passwordInputStatus).toBeInTheDocument();
     expect(emailInputStatus).toHaveAttribute('title', 'Required field');
+  });
+
+  test('Should call Validation with correct email', async() => {
+    const { sut, validationSpy } = makeSut();
+    const user = userEvent.setup();
+
+    const emailInput = sut.getByTestId('email');
+    await user.type(emailInput, 'any_email');
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
+    
+    const passwordInput = sut.getByTestId('password');
+    await user.type(passwordInput, 'any_password');
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
   })
  })
