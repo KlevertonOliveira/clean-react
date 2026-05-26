48baeaf03308e85afb56a0f2996b2a1438124c85
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-05-26 20:21:33 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldError } from "@/validation/errors";
 import { RequiredFieldValidation } from "./required-field-validation";
+import { faker } from '@faker-js/faker';
 
 describe('RequiredFieldValidation', () => { 
   test ('Should return error message if field is empty', () => {
@@ -7,5 +8,12 @@ describe('RequiredFieldValidation', () => {
     
     const errorMessage = sut.validate('');
     expect(errorMessage).toEqual(new RequiredFieldError());
+  })
+  
+  test ('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email');
+    
+    const errorMessage = sut.validate(faker.lorem.word());
+    expect(errorMessage).toBeFalsy();
   })
  })
