85578aace94f901f3064bbbdf103a540d8805cc4
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-05-27 16:47:30 -0300

    feat: ensure MinLengthValidation returns error if value length is invalid

---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,13 @@
+import { InvalidSizeError } from "@/validation/errors";
+import type { FieldValidation } from "@/validation/protocols/field-validation";
+
+export class MinLengthValidation implements FieldValidation {
+  constructor(
+    readonly field: string, 
+    private readonly minLength: number
+  ){}
+
+  validate(value: string): Error | null {
+    return value ? new InvalidSizeError() : null
+  }
+}
