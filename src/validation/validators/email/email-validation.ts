453d02f5fb6284e0500a154cb98e000d7c4afcf3
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-05-27 14:40:42 -0300

    feat: ensure EmailValidation returns null if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,6 +5,8 @@ export class EmailValidation implements FieldValidation {
   constructor(readonly field: string){}
 
   validate(value: string): Error | null {
-    return value ? null : new InvalidFieldError();
+    const emailRegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+    return emailRegExp.test(value) ? null : new InvalidFieldError();
   }
 }
