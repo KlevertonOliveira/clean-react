8148fa5d64760781658311e3bfa50be5e286b688
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-06-02 19:48:01 -0300

    fix: ensure AxiosHttpClient uses correct types instead of 'unknown'

--- src/infra/axios-http-client/axios-http-client.ts
+++ src/infra/axios-http-client/axios-http-client.ts
@@ -1,9 +1,11 @@
 import type { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http';
+import type { AccountModel } from "@/domain/models";
+import type { AuthenticationParams } from "@/domain/usecases";
 import axios from 'axios';
 
-export class AxiosHttpClient implements HttpPostClient<unknown, unknown>{
+export class AxiosHttpClient implements HttpPostClient<AuthenticationParams, AccountModel>{
   
-  async post(params: HttpPostParams<unknown>): Promise<HttpResponse<unknown>> {
+  async post(params: HttpPostParams<AuthenticationParams>): Promise<HttpResponse<AccountModel>> {
     const httpResponse = await axios.post(params.url, params.body);
     
     return {
