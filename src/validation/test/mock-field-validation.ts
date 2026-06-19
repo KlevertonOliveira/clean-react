ce6aae3cc755e599f7ace87402145779b575c2e6
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-06-19 10:09:10 -0300

    fix: correct minor issues to allow project build

--- src/validation/test/mock-field-validation.ts
+++ src/validation/test/mock-field-validation.ts
@@ -4,9 +4,9 @@ import type { FieldValidation } from "@/validation/protocols/field-validation";
 export class FieldValidationSpy implements FieldValidation {
   error: Error | null = null;
 
-  constructor(readonly field: string) {}
+  constructor(readonly field: string) { }
 
-  validate(value: string): Error | null {
+  validate(_value: string): Error | null {
     return this.error;
   }
 }
