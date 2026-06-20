dc1daedb00e36afa8e6103d98bd7a766eb1e9615
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-06-20 12:45:50 -0300

    feat: add SaveAccessToken to Login composition

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,12 +1,14 @@
 import { LoginPage } from "@/presentation/pages";
-import { makeRemoteAuthentication } from "@/main/factories/usecases/authentication/remote-authentication-factory"
+import { makeRemoteAuthentication } from "@/main/factories/usecases/authentication/remote-authentication-factory";
+import { makeLocalSaveAccessToken } from "@/main/factories/usecases/save-access-token/save-access-token-factory";
 import { makeLoginValidation } from "./login-validation-factory";
 
 export default function MakeLogin(): React.JSX.Element {
   return (
-    <LoginPage 
+    <LoginPage
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
-  )
+  );
 }
