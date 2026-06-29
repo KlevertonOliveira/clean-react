86c9e5c27fd4a0ef0c5c1c0cba07637bbbab9917
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-06-29 17:32:10 -0300

    test: ensure name input status shows error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,22 +1,54 @@
-import { render, type RenderResult } from "@testing-library/react";
+import { cleanup, render, type RenderResult } from "@testing-library/react";
 import SignUpPage from "./signup";
+import { ValidationSpy } from "@/presentation/test";
+import { faker } from "@faker-js/faker";
+import userEvent from "@testing-library/user-event";
 
 type SutTypes = {
   sut: RenderResult;
 };
 
-const makeSut = (): SutTypes => {
-  const sut = render(<SignUpPage />);
+type SutParams = {
+  validationError: string;
+};
+
+const makeSut = (params?: SutParams): SutTypes => {
+  const validationSpy = new ValidationSpy();
+  validationSpy.errorMessage = params?.validationError ?? '';
+
+  const sut = render(<SignUpPage validation={validationSpy} />);
 
   return {
     sut,
   };
 };
 
+const populateField = async (
+  sut: RenderResult,
+  fieldName: string,
+  value = faker.lorem.word()
+): Promise<void> => {
+  const user = userEvent.setup();
+  const fieldInput = sut.getByTestId(fieldName);
+  await user.type(fieldInput, value);
+};
+
+const testStatusForField = (
+  sut: RenderResult,
+  field: string,
+  validationError: string,
+) => {
+  const fieldStatus = sut.getByTestId(field + '-status');
+  expect(fieldStatus).toBeInTheDocument();
+  expect(fieldStatus.title).toBe(validationError);
+};
+
 describe("Login Component", () => {
+  afterEach(cleanup);
+
   test("Should start with initial state", async () => {
     const validationError = "Required field";
-    const { sut } = makeSut();
+    const { sut } = makeSut({ validationError });
     await sut.findByTestId("signup-form");
 
     const spinner = sut.queryByTestId("spinner");
@@ -31,9 +63,18 @@ describe("Login Component", () => {
     const fields = ["name", "email", "password", "confirm-password"];
 
     for (const field of fields) {
-      const fieldStatus = sut.getByTestId(field + '-status');
-      expect(fieldStatus).toBeInTheDocument();
-      expect(fieldStatus.title).toBe(validationError);
+      testStatusForField(sut, field, validationError);
     }
+  });
+
+  test("Should show name error if Validation fails", async () => {
+    const field = "name";
+    const validationError = faker.lorem.word();
+
+    const { sut } = makeSut({ validationError });
+    await sut.findByTestId("signup-form");
+
+    await populateField(sut, field);
+    testStatusForField(sut, field, validationError);
   });
 });
