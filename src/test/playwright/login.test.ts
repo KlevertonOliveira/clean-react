495f0b7f23763d091d743e8df7694d5f28eb381a
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-07-13 20:33:44 -0300

    test: ensure Login present error state if form is invalid

--- src/test/playwright/login.test.ts
+++ src/test/playwright/login.test.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import { test, expect } from "@playwright/test";
 test.describe("Login", () => {
 
@@ -13,6 +14,17 @@ test.describe("Login", () => {
 
     await expect(page.getByTestId("password-status")).toBeVisible();
     await expect(page.getByTestId("password-status")).toHaveAttribute("title", "Required field");
+
+    await expect(page.getByTestId("submit-button")).toBeDisabled();
+    await expect(page.getByTestId("formErrorMessage")).not.toBeVisible();
+  });
+
+  test("Should present error state if form is invalid", async ({ page }) => {
+    await page.getByTestId("email").fill(faker.lorem.word());
+    await expect(page.getByTestId("email-status")).toHaveAttribute("title", "Invalid field value");
+
+    await page.getByTestId("password").fill(faker.lorem.word());
+    await expect(page.getByTestId("password-status")).toHaveAttribute("title", "Invalid size error!");
 
     await expect(page.getByTestId("submit-button")).toBeDisabled();
     await expect(page.getByTestId("formErrorMessage")).not.toBeVisible();
