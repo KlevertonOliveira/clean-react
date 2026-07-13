0df8b785e8fadc71d3b22f13e87b923ddd975136
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-07-13 20:17:49 -0300

    test: ensure Login starts with correct initial state

---
+++ src/test/playwright/login.test.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+test.describe("Login", () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173/login");
+  });
+
+  test("Should load with correct initial state", async ({ page }) => {
+    await expect(page).toHaveTitle("4Dev - Surveys for programmers");
+
+    await expect(page.getByTestId("email-status")).toBeVisible();
+    await expect(page.getByTestId("email-status")).toHaveAttribute("title", "Required field");
+
+    await expect(page.getByTestId("password-status")).toBeVisible();
+    await expect(page.getByTestId("password-status")).toHaveAttribute("title", "Required field");
+
+    await expect(page.getByTestId("submit-button")).toBeDisabled();
+    await expect(page.getByTestId("formErrorMessage")).not.toBeVisible();
+  });
+});
