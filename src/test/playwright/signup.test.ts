17adf81195c36c5c13618a56133593e10b2dc86b
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-07-23 14:37:17 -0300

    test: ensure SignUp page presents error state if form is invalid

--- src/test/playwright/signup.test.ts
+++ src/test/playwright/signup.test.ts
@@ -1,4 +1,5 @@
 
+import { faker } from "@faker-js/faker";
 import { test, expect } from "@playwright/test";
 test.describe("SignUp", () => {
 
@@ -19,6 +20,23 @@ test.describe("SignUp", () => {
 
     await expect(page.getByTestId("confirmPassword-error-status")).toBeVisible();
     await expect(page.getByTestId("confirmPassword-error-status")).toHaveAttribute("title", "Required field");
+
+    await expect(page.getByTestId("submit-button")).toBeDisabled();
+    await expect(page.getByTestId("formErrorMessage")).not.toBeVisible();
+  });
+
+  test("Should present error state if form is invalid", async ({ page }) => {
+    await page.getByTestId("name").fill(faker.string.alphanumeric(3));
+    await expect(page.getByTestId("name-error-status")).toHaveAttribute("title", "Invalid field size (minimum: 5)");
+
+    await page.getByTestId("email").fill(faker.lorem.word());
+    await expect(page.getByTestId("email-error-status")).toHaveAttribute("title", "Invalid field value");
+
+    await page.getByTestId("password").fill(faker.string.alphanumeric(3));
+    await expect(page.getByTestId("password-error-status")).toHaveAttribute("title", "Invalid field size (minimum: 8)");
+
+    await page.getByTestId("confirmPassword").fill(faker.string.alphanumeric(2));
+    await expect(page.getByTestId("confirmPassword-error-status")).toHaveAttribute("title", "Invalid field value");
 
     await expect(page.getByTestId("submit-button")).toBeDisabled();
     await expect(page.getByTestId("formErrorMessage")).not.toBeVisible();
