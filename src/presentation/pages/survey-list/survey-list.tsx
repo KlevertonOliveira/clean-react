7cc73a0950050a69cc6994fa8f86d814c4cea461
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-18 14:57:57 -0300

    feat: add survey icon to show status

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,5 +1,8 @@
+{/* eslint-disable no-constant-condition */ }
+
 import { Logo } from "@/presentation/components";
 import Footer from "@/presentation/components/footer/footer";
+import { CheckIcon, QuestionMarkIcon } from "@phosphor-icons/react";
 
 export default function SurveyListPage() {
   return (
@@ -22,101 +25,18 @@ export default function SurveyListPage() {
         </h2>
 
         <ul className="flex flex-wrap justify-between">
-          <li className="h-60 bg-white flex flex-col justify-between rounded-lg basis-[48%] mb-6 shadow">
+          <li className="h-60 bg-white flex flex-col justify-between rounded-lg basis-[48%] mb-6 shadow relative">
 
-            <div className="flex justify-between rounded-lg grow 
+            <div className="flex justify-between rounded-lg grow
             bg-[linear-gradient(to_right,rgb(from_var(--color-primaryLight)_r_g_b/20%)_0_12.5%,var(--color-white)_12.5%_100%)]
             bg-no-repeat 
             bg-size-[100%_100%]"
             >
-
-              <time className="flex flex-col bg-primaryLight text-white rounded-lg ml-4 w-15 h-25 items-center self-center justify-center shrink-0">
-                <span className="text-4xl font-bold">22</span>
-                <span className="lowercase m-0 mb-1">Mar</span>
-                <span className="">2020</span>
-              </time>
-
-              <p className="text-xl m-6 self-center">What&apos;s your favorite web framework?</p>
-            </div>
-
-            <footer className="bg-primary hover:bg-primaryDark text-white leading-10 lowercase text-center cursor-pointer rounded-b-lg"
-            >
-              See Result
-            </footer>
-          </li>
-          <li className="h-60 bg-white flex flex-col justify-between rounded-lg basis-[48%] mb-6 shadow">
-
-            <div className="flex justify-between rounded-lg grow 
-            bg-[linear-gradient(to_right,rgb(from_var(--color-primaryLight)_r_g_b/20%)_0_12.5%,var(--color-white)_12.5%_100%)]
-            bg-no-repeat 
-            bg-size-[100%_100%]"
-            >
-
-              <time className="flex flex-col bg-primaryLight text-white rounded-lg ml-4 w-15 h-25 items-center self-center justify-center shrink-0">
-                <span className="text-4xl font-bold">22</span>
-                <span className="lowercase m-0 mb-1">Mar</span>
-                <span className="">2020</span>
-              </time>
-
-              <p className="text-xl m-6 self-center">What&apos;s your favorite web framework?</p>
-            </div>
-
-            <footer className="bg-primary hover:bg-primaryDark text-white leading-10 lowercase text-center cursor-pointer rounded-b-lg"
-            >
-              See Result
-            </footer>
-          </li>
-          <li className="h-60 bg-white flex flex-col justify-between rounded-lg basis-[48%] mb-6 shadow">
-
-            <div className="flex justify-between rounded-lg grow 
-            bg-[linear-gradient(to_right,rgb(from_var(--color-primaryLight)_r_g_b/20%)_0_12.5%,var(--color-white)_12.5%_100%)]
-            bg-no-repeat 
-            bg-size-[100%_100%]"
-            >
-
-              <time className="flex flex-col bg-primaryLight text-white rounded-lg ml-4 w-15 h-25 items-center self-center justify-center shrink-0">
-                <span className="text-4xl font-bold">22</span>
-                <span className="lowercase m-0 mb-1">Mar</span>
-                <span className="">2020</span>
-              </time>
-
-              <p className="text-xl m-6 self-center">What&apos;s your favorite web framework?</p>
-            </div>
-
-            <footer className="bg-primary hover:bg-primaryDark text-white leading-10 lowercase text-center cursor-pointer rounded-b-lg"
-            >
-              See Result
-            </footer>
-          </li>
-          <li className="h-60 bg-white flex flex-col justify-between rounded-lg basis-[48%] mb-6 shadow">
-
-            <div className="flex justify-between rounded-lg grow 
-            bg-[linear-gradient(to_right,rgb(from_var(--color-primaryLight)_r_g_b/20%)_0_12.5%,var(--color-white)_12.5%_100%)]
-            bg-no-repeat 
-            bg-size-[100%_100%]"
-            >
-
-              <time className="flex flex-col bg-primaryLight text-white rounded-lg ml-4 w-15 h-25 items-center self-center justify-center shrink-0">
-                <span className="text-4xl font-bold">22</span>
-                <span className="lowercase m-0 mb-1">Mar</span>
-                <span className="">2020</span>
-              </time>
-
-              <p className="text-xl m-6 self-center">What&apos;s your favorite web framework?</p>
-            </div>
-
-            <footer className="bg-primary hover:bg-primaryDark text-white leading-10 lowercase text-center cursor-pointer rounded-b-lg"
-            >
-              See Result
-            </footer>
-          </li>
-          <li className="h-60 bg-white flex flex-col justify-between rounded-lg basis-[48%] mb-6 shadow">
-
-            <div className="flex justify-between rounded-lg grow 
-            bg-[linear-gradient(to_right,rgb(from_var(--color-primaryLight)_r_g_b/20%)_0_12.5%,var(--color-white)_12.5%_100%)]
-            bg-no-repeat 
-            bg-size-[100%_100%]"
-            >
+              <div className={`absolute -top-2 -right-2 text-white rounded-full 
+                ${true ? "bg-red-600" : "bg-green-600"}`}
+              >
+                {true ? <QuestionMarkIcon size={32} /> : <CheckIcon size={32} />}
+              </div>
 
               <time className="flex flex-col bg-primaryLight text-white rounded-lg ml-4 w-15 h-25 items-center self-center justify-center shrink-0">
                 <span className="text-4xl font-bold">22</span>
