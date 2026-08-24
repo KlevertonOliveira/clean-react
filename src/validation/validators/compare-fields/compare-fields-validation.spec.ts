b36cad26b004f55658571cd69ff823c2b91bba6c
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-24 10:37:31 -0300

    fix: change CompareFieldsValidation test to ensure equal values will never be used

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -12,8 +12,8 @@ describe('CompareFieldsValidation', () => {
     const fieldToCompare = faker.database.column();
     const sut = makeSut(field, fieldToCompare);
     const error = sut.validate({
-      [field]: faker.lorem.word(),
-      [fieldToCompare]: faker.lorem.word()
+      [field]: faker.lorem.words(3),
+      [fieldToCompare]: faker.lorem.words(4)
     });
     expect(error).toEqual(new InvalidFieldError());
   });
