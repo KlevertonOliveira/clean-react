ffefddc846e63c9f3aca25f4f80837377bbf4360
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-26 17:11:55 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { SurveyListPage } from "@/presentation/pages";
 
+const makeSut = (): void => {
+  render(<SurveyListPage />);
+};
+
 describe('SurveyList Component', () => {
   test('Should present 4 skeleton items on start', () => {
-    render(<SurveyListPage />);
+    makeSut();
+
     const surveyList = screen.getByTestId("survey-list");
     const listItems = surveyList.getElementsByTagName("li");
+
     expect(listItems).toHaveLength(4);
   });
 });
