f991a6573ac125a00b553321113da91b81a0c8d8
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-28 12:20:51 -0300

    feat: add TanStack's QueryClientProvider to app

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,17 @@ import { routeAuth } from "./utils/route-auth";
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 // Create a new router instance
 const router = createRouter({
   routeTree,
-  context: { routeAuth }
+  context: {
+    routeAuth,
+    queryClient
+  }
 });
 
 // Register the router instance for type safety
@@ -25,7 +31,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import type { routeAuth } from "@/utils/route-auth";
+import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
@@ -12,6 +13,7 @@ const RootLayout = () => (
 
 export type RouterContext = {
   routeAuth: typeof routeAuth;
+  queryClient: QueryClient;
 };
 
 export const Route = createRootRouteWithContext<RouterContext>()({ component: RootLayout });
