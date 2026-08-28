fb6f3700a5c753503280fe988a973b63f021e638
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-28 17:38:27 -0300

    test: ensure SurveyListPage renders error on failure

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -4,6 +4,7 @@ import type { LoadSurveyList } from "@/domain/usecases";
 import type { SurveyModel } from "@/domain/models";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { mockSurveyList } from "@/domain/test";
+import { UnexpectedError } from "@/domain/errors";
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0;
@@ -20,9 +21,14 @@ type SutTypes = {
 };
 
 
-const makeSut = (): SutTypes => {
-  const queryClient = new QueryClient();
-  const loadSurveyListSpy = new LoadSurveyListSpy();
+const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false
+      }
+    }
+  });
 
   render(
     <QueryClientProvider client={queryClient}>
@@ -36,10 +42,11 @@ const makeSut = (): SutTypes => {
 };
 
 describe('SurveyList Component', () => {
-  test('Should present 4 skeleton items on start', () => {
+  test('Should present 4 skeleton items on start', async () => {
     makeSut();
     const listItems = screen.getAllByTestId("skeleton-survey-item");
     expect(listItems).toHaveLength(4);
+    expect(screen.queryByTestId("error-message")).not.toBeInTheDocument();
   });
 
   test('Should call LoadSurveyList', () => {
@@ -51,5 +58,17 @@ describe('SurveyList Component', () => {
     makeSut();
     const surveyItems = await screen.findAllByTestId("survey-item");
     expect(surveyItems).toHaveLength(3);
+    expect(screen.queryByTestId("error-message")).not.toBeInTheDocument();
+  });
+
+  test("Should render error message on failure", async () => {
+    const loadSurveyListSpy = new LoadSurveyListSpy();
+    const error = new UnexpectedError();
+    vi.spyOn(loadSurveyListSpy, "loadAll").mockRejectedValueOnce(error);
+
+    makeSut(loadSurveyListSpy);
+
+    expect(await screen.findByTestId("error-message")).toHaveTextContent(error.message);
+    expect(screen.queryByTestId("survey-list")).not.toBeInTheDocument();
   });
 });
