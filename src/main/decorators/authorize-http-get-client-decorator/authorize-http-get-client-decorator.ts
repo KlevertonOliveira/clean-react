a43c962ea0c345cfbea429b54120cc6e5eea7494
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-30 11:42:53 -0300

    feat: ensure AuthorizeHttpGetClientDecorator does not add token if GetStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -1,12 +1,16 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+ 
 import type { GetStorage } from "@/data/protocols/cache";
-import type { HttpGetParams } from "@/data/protocols/http";
+import type { HttpGetClient, HttpGetParams } from "@/data/protocols/http";
 
 export class AuthorizeHttpGetClientDecorator {
-  constructor(private readonly getStorage: GetStorage) { }
+  constructor(
+    private readonly getStorage: GetStorage,
+    private readonly httpGetClient: HttpGetClient<unknown>
+  ) { }
 
-  async get(_params: HttpGetParams): Promise<null> {
+  async get(params: HttpGetParams): Promise<null> {
     this.getStorage.get("account");
+    await this.httpGetClient.get(params);
     return null;
   }
 }
