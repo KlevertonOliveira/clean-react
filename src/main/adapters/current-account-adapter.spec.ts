fcb2f967244973f87e63507a2a97f5aed57aef7d
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-31 11:57:36 -0300

    test: ensure LocalStorageAdapter.set is called with correct values

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,8 +1,15 @@
 import { mockAccountModel } from "@/domain/test";
 import { LocalStorageAdapter } from "@/infra/cache/local-storage-adapter";
-import { getCurrentAccountAdapter } from "./current-account-adapter";
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from "./current-account-adapter";
 
 describe("CurrentAccountAdapter", () => {
+  test("Should call LocalStorageAdapter.set with correct values", () => {
+    const account = mockAccountModel();
+    const setSpy = vi.spyOn(LocalStorageAdapter.prototype, "set");
+    setCurrentAccountAdapter(account);
+    expect(setSpy).toHaveBeenCalledWith("account", account);
+  });
+
   test("Should call LocalStorageAdapter.get with correct values", () => {
     const mockedAccount = mockAccountModel();
 
