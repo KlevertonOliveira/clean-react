d75fefb59628b18829441f0ceeaf6bb0836ff5df
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-31 10:25:54 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.tsx
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.tsx
@@ -1,7 +1,8 @@
+import { makeApiURL } from "@/main/factories/http";
+import { makeAuthorizeHttpGetClientDecorator } from "@/main/factories/decorators";
 import { RemoteLoadSurveyList } from "@/data/usecases/load-survey-list/remote-load-survey-list";
 import type { LoadSurveyList } from "@/domain/usecases";
-import { makeApiURL, makeAxiosHttpClient } from "../../http";
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiURL("/surveys"), makeAxiosHttpClient());
+  return new RemoteLoadSurveyList(makeApiURL("/surveys"), makeAuthorizeHttpGetClientDecorator());
 };
