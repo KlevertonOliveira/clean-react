18a0131932fdae7ca3275b87cb7e3291aadd220c
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-08-31 11:57:12 -0300

    feat: ensure LocalStorageAdapter.set is called with correct values

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,5 +1,9 @@
 import type { AccountModel } from "@/domain/models";
-import { makeLocalStorageAdapter } from "../factories/cache/local-storage-adapter-factory";
+import { makeLocalStorageAdapter } from "@/main/factories/cache";
+
+export const setCurrentAccountAdapter = (account: AccountModel): void => {
+  makeLocalStorageAdapter().set("account", account);
+};
 
 export const getCurrentAccountAdapter = (): AccountModel => {
   return makeLocalStorageAdapter().get("account");
