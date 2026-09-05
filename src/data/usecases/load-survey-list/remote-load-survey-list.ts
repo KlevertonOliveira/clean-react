1968cd80429183e8298730a6a6e8204ec4c4a5fb
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-09-05 18:08:21 -0300

    feat: ensure RemoteLoadSurveyList throws AccessDenied error on 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,5 +1,5 @@
 import { HttpStatusCode, type HttpGetClient } from "@/data/protocols/http";
-import { UnexpectedError } from "@/domain/errors";
+import { AccessDeniedError, UnexpectedError } from "@/domain/errors";
 import type { SurveyModel } from "@/domain/models";
 import type { LoadSurveyList } from "@/domain/usecases";
 
@@ -19,6 +19,7 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
         remoteSurveys?.map(survey => ({ ...survey, date: new Date(survey.date) }))
       );
       case HttpStatusCode.noContent: return [];
+      case HttpStatusCode.forbidden: throw new AccessDeniedError();
       default: throw new UnexpectedError();
     }
   }
