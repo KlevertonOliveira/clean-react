2d1de8d9bff0d48df96212b83d7c8eedf7e4d01c
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-09-07 15:35:51 -0300

    test: ensure AuthNavigation calls logout and navigates to '/login' when forbidden event is emitted

---
+++ src/presentation/router/auth-navigation/auth-navigation.spec.ts
@@ -0,0 +1,24 @@
+import { createAuthEventEmitter } from "@/infra/auth/auth-event-emitter";
+import { setupAuthNavigation } from "@/presentation/router/auth-navigation/auth-navigation";
+import type { AppRouter } from "@/presentation/router/router";
+
+describe('AuthNavigation', () => {
+  test('Should logout and navigate to /login when forbidden event is emitted', () => {
+    const authEventEmitter = createAuthEventEmitter();
+
+    const logoutMock = vi.fn();
+    const navigateMock = vi.fn();
+
+    const router = { navigate: navigateMock } as unknown as AppRouter;
+
+    setupAuthNavigation({
+      router,
+      onLogout: logoutMock,
+      authEventEmitter
+    });
+    authEventEmitter.emit({ type: "forbidden" });
+
+    expect(logoutMock).toHaveBeenCalled();
+    expect(navigateMock).toHaveBeenCalledWith({ to: "/login" });
+  });
+});
