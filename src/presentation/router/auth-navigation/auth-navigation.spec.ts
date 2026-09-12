e6e90cc8ef0bb9672ebcb1f545212528c2269ed2
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2026-09-12 10:02:02 -0300

    test: ensure logout and navigation to /login are not triggered when emitted event is not of type 'forbidden'

--- src/presentation/router/auth-navigation/auth-navigation.spec.ts
+++ src/presentation/router/auth-navigation/auth-navigation.spec.ts
@@ -2,8 +2,8 @@ import { createAuthEventEmitter } from "@/infra/auth/auth-event-emitter";
 import { setupAuthNavigation } from "@/presentation/router/auth-navigation/auth-navigation";
 import type { AppRouter } from "@/presentation/router/router";
 
-describe('AuthNavigation', () => {
-  test('Should logout and navigate to /login when forbidden event is emitted', () => {
+describe("AuthNavigation", () => {
+  test("Should logout and navigate to /login when forbidden event is emitted", () => {
     const authEventEmitter = createAuthEventEmitter();
 
     const logoutMock = vi.fn();
@@ -22,7 +22,7 @@ describe('AuthNavigation', () => {
     expect(navigateMock).toHaveBeenCalledWith({ to: "/login" });
   });
 
-  test('Should not logout and navigate to /login after unsubscribe', () => {
+  test("Should not logout and navigate to /login after unsubscribe", () => {
     const authEventEmitter = createAuthEventEmitter();
 
     const logoutMock = vi.fn();
@@ -39,6 +39,27 @@ describe('AuthNavigation', () => {
     unsubscribe();
 
     authEventEmitter.emit({ type: "forbidden" });
+
+    expect(logoutMock).not.toHaveBeenCalled();
+    expect(navigateMock).not.toHaveBeenCalled();
+  });
+
+  test("Should not logout and navigate to /login when emitted event is not of type 'forbidden'", () => {
+    const authEventEmitter = createAuthEventEmitter();
+
+    const logoutMock = vi.fn();
+    const navigateMock = vi.fn();
+
+    const router = { navigate: navigateMock } as unknown as AppRouter;
+
+    setupAuthNavigation({
+      router,
+      onLogout: logoutMock,
+      authEventEmitter
+    });
+
+    /* @ts-expect-error / type "random" does not exist */
+    authEventEmitter.emit({ type: "random" });
 
     expect(logoutMock).not.toHaveBeenCalled();
     expect(navigateMock).not.toHaveBeenCalled();
